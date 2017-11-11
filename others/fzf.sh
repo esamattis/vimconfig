8518fcf7db23b3aa21cf9122bf9d0f6b8067a62a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-11-11 11:35:24 +0000

    drop git from fzf

--- others/fzf.sh
+++ others/fzf.sh
@@ -1,13 +1,8 @@
 
 if [ -x $(which ag) ]; then
-    export FZF_DEFAULT_COMMAND='
-      (git ls-tree -r --name-only HEAD ||
-      ag -g "") 2> /dev/null'
+    export FZF_DEFAULT_COMMAND='ag -g ""'
 else
-    export FZF_DEFAULT_COMMAND='
-      (git ls-tree -r --name-only HEAD ||
-       find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
-          sed s/^..//) 2> /dev/null'
+    export FZF_DEFAULT_COMMAND='find . -path "*/\.*" -prune -o -type f -print -o -type l -print'
 fi
 
 
