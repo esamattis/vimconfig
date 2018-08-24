359dcbf9d676826aa10abfcc318431ac1ade1a20
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-08-24 12:38:08 +0300

    Update yats.vim to 0f65e839ed7fa987c38edf6a4862b5dd48046caa

---
+++ plugged/yats.vim/test/test.ts
@@ -0,0 +1,79 @@
+var p: Promise<void>
+
+var a = { template: /*eee*/ this.template
+}
+
+const COMPILER_PROVIDERS: Array<any[]> = null
+
+var a = {
+  get method<T>(a) {
+  },
+}
+
+var a = [/\/\//, 123]
+
+
+if (a < (<number>b))
+    a
+}
+
+var a: T[K] = 1213
+
+switch (eww) {
+  case 1222
+}
+
+var a = 123
+
+class MyClass<string> extends Mixin({test: 123}) {
+    noed // ewewe
+    a = '3' // ewew
+    get(arg: string) {
+    }
+}
+
+var a = test[333]
+
+function test({a = process.stdout}) {}
+
+// test
+var a = 1/3
+/* test*/
+type A = 42
+
+
+test({
+  a: test ? // test
+  test: ee
+})
+
+getSymbolIterator(3) in obj;
+
+
+list.reduce((flat: any[], item: T | T[]): T[] => {
+  const flatItem = Array.isArray(item) ? flatten(item) : item;
+  return (<T[]>flat).concat(flatItem);
+}, []);
+
+export function noProviderError(injector: ReflectiveInjector, key: ReflectiveKey): InjectionError {
+  return injectionError(injector, key, function(keys: ReflectiveKey[]) {
+    const first = stringify(keys[0].token);
+    return `No provider for ${first}!${constructResolvingPath(keys)}`;
+  });
+}
+
+
+export class QueryList<T>/* implements Iterable<T> */ {
+  public readonly dirty = true;
+}
+
+
+class A {
+    a = (a: () => void) => { }
+}
+
+var a = ({a: () => 123})
+
+enum A {
+
+}
